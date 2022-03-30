7892f37cf61227e54fb4b9b6a355f304fd87e321
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-03-30 12:17:22 -0700

    adds number 5 solution

--- practice.js
+++ practice.js
@@ -44,9 +44,15 @@ function beginWithA(strings) {
   });
   return count;
 }
-console.log(beginWithA(['apple', 'banana', 'around', 'log', 'animal']));
+// console.log(beginWithA(['apple', 'banana', 'around', 'log', 'animal']));
 
 // 5) Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+function sepratedByCommas(strings) {
+  return strings.join(",");
+}
+console.log(sepratedByCommas(['a', 'p', 'p', 'l', 'e', 's']));
+
+
 // 6) Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
 // 7) Write a function that takes in an array of numbers and returns the two smallest numbers.
 // 8) Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
